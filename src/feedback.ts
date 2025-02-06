9c36eed26e496fdf5aac8f7ce8776f198899d5be
Feng Zhou <fengzhou@gmail.com>
2025-02-07 00:31:56 +0800

    refactor: Update feedback generation to use new model and output format

--- src/feedback.ts
+++ src/feedback.ts
@@ -1,7 +1,8 @@
-import { generateObject } from 'ai';
+import { generateObject, LanguageModelV1 } from 'ai';
 import { z } from 'zod';
 
-import { o3MiniModel } from './ai/providers';
+// import { o3MiniModel } from './ai/providers';
+import { model } from './ai/providers';
 import { systemPrompt } from './prompt';
 
 export async function generateFeedback({
@@ -12,17 +13,26 @@ export async function generateFeedback({
   numQuestions?: number;
 }) {
   const userFeedback = await generateObject({
-    model: o3MiniModel,
+    model: model,
+    output: 'no-schema',
     system: systemPrompt(),
-    prompt: `Given the following query from the user, ask some follow up questions to clarify the research direction. Return a maximum of ${numQuestions} questions, but feel free to return less if the original query is clear: <query>${query}</query>`,
-    schema: z.object({
-      questions: z
-        .array(z.string())
-        .describe(
-          `Follow up questions to clarify the research direction, max of ${numQuestions}`,
-        ),
-    }),
+    prompt: `Given the following query from the user, ask some follow up questions to clarify the research direction. Return a maximum of ${numQuestions} questions, but feel free to return less if the original query is clear: <query>${query}</query>
+    Example JSON output:
+    {
+      "questions": [
+        "A follow-up question",
+      ]
+    }
+    `,
+    // schema: z.object({
+    //   questions: z
+    //     .array(z.string())
+    //     .describe(
+    //       `Follow up questions to clarify the research direction, max of ${numQuestions}`,
+    //     ),
+    // }),
   });
+  console.log("Feedback questions: ", userFeedback.object);
 
   return userFeedback.object.questions.slice(0, numQuestions);
 }
